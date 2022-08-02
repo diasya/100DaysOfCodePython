6c1e7ff0f36723a3e7ba90ec70e55c5bf6e1b57e
diasya <mustafasaiday@gmail.com>
2022-08-02 19:01:09 +0300

    Calculator with good UX

--- AmazingCalculator.py
+++ AmazingCalculator.py
@@ -1,36 +1,44 @@
-#Add
+# Add
 def add(n1, n2):
-  return n1 + n2
+    return n1 + n2
 
-#Subtract
+
+# Subtract
 def subtract(n1, n2):
-  return n1 - n2
+    return n1 - n2
 
-#Multiply
+
+# Multiply
 def multiply(n1, n2):
-  return n1 * n2
+    return n1 * n2
 
-#Divide
+
+# Divide
 def divide(n1, n2):
-  return n1 / n2
+    return n1 / n2
+
 
 operations = {
-  "+": add,
-  "-": subtract,
-  "*": multiply,
-  "/": divide
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
 }
 
 num1 = int(input("Enter first number: "))
 
-
 for key in operations:
-  print(key)
+    print(key)
 operation = input("choose an operation to do: ")
 num2 = int(input("Enter second number: "))
 function = operations[operation]
-answer = function(num1,num2)
+answer_first = function(num1, num2)
 
-print(f"{num1} {operation} {num2} = {answer}")
+print(f"{num1} {operation} {num2} = {answer_first}")
 
+operation = input("choose another operation to do: ")
+num3 = int(input("Enter the number: "))
+function = operations[operation]
+answer_second = function(num3, answer_first)
 
+print(f"{answer_first} {operation} {num3} = {answer_second}")
